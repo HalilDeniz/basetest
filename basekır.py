d522ab6584668ab9177bb0055a9f78ef489c6e28
HalilDeniz <62566319+HalilDeniz@users.noreply.github.com>
2020-03-27 21:09:31 +0300

    Add files via upload

---
+++ basekır.py
@@ -0,0 +1,62 @@
+import sys
+import base64
+def çık():
+	print("çıkılıyor")
+	sys.exit()
+def usage():
+	print("""
+	 _                     _    _  _
+	| |__   __ _ ___  ___ / /_ | || |
+	| '_ \ / _` / __|/ _ \ '_ \| || |_
+	| |_) | (_| \__ \  __/ (_) |__   _|
+	|_.__/ \__,_|___/\___|\___/   |_|
+
+         """)
+
+
+	print(f'{sys.argv[0]} -e or --encode <"encode"> >>>> şifreler')
+	print(f'{sys.argv[0]} -d or --decode <"decode"> >>>> şifreyi çözer')
+	print(f"{sys.argv[0]} -fe or --fileencode <encode> >>>> dosyadaki karakteri şifreler")
+	print(f"{sys.argv[0]} -fd or --filedecode <decode> >>>> dosyadaki şifreyi çözer")
+	sys.exit(1)
+
+if len(sys.argv) <3:
+	usage()
+	sys.argv(1)
+
+elif sys.argv[1] in ["-e","--encode"]:
+	karakter = sys.argv[2]
+	çevir = karakter.encode("utf-8")
+	göster = base64.b64encode(çevir)
+	string = göster.decode("utf-8")	
+	print(string)
+
+elif sys.argv[1] in ["-d","--decode"]:
+        karakter1 = base64.b64decode(sys.argv[2])
+        print(karakter1.decode("utf-8"))
+
+elif sys.argv[1] in ["-fe","--fileencode"]:
+	try:
+		dosya = open(sys.argv[2])
+		aç = dosya.readlines()
+		for oku in aç:
+			karakter = oku.encode("utf-8")
+			göster = base64.b64encode(karakter)
+			string = göster.decode("utf-8")
+			print(string)
+	except FileNotFoundError:
+		print("böyle bir dosya yok")
+elif sys.argv[1] in ["-fd","--filedecode"]:
+	try:
+		dosya = open(sys.argv[2])
+		aç = dosya.readlines()
+		for oku in aç:
+			karakter = base64.b64decode(oku)
+			print(karakter.decode("utf-8"))
+	except FileNotFoundError:
+		print("böyle bir dosya yok")
+
+else:
+	print(f"Girdiğiniz panametre ({sys.argv[1]}) anlaşılmadı")
+	usage()
+	çık()
