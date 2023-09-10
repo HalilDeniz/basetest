540ecd8e255a8376e4598e81b652856c01958ff8
halil ibrahim deniz <62566319+HalilDeniz@users.noreply.github.com>
2023-09-10 13:21:36 +0300

    Update basekır.py

--- basekır.py
+++ basekır.py
@@ -1,72 +1,91 @@
-try:
-        import sys
-        import base64
-        import os
-except ModuleNotFoundError:
-        print("eksik modul yükleniyor")
-        os.system("pip3 install base64")
-        os.system("pip install base64")
-        os.system("clear")
-        print("Lütfen tekrar deneyin")
-        usage()
-def çık():
-	print("çıkılıyor")
-	sys.exit()
-def usage():
-	print("""
-	 _                     _    _  _
-	| |__   __ _ ___  ___ / /_ | || |
-	| '_ \ / _` / __|/ _ \ '_ \| || |_
-	| |_) | (_| \__ \  __/ (_) |__   _|
-	|_.__/ \__,_|___/\___|\___/   |_|
+import sys
+import base64
+import os
+import argparse
 
-         """)
+class Base64EncoderDecoder:
+    def __init__(self):
+        try:
+            import base64
+        except ModuleNotFoundError:
+            print("eksik modul yükleniyor")
+            os.system("pip3 install base64")
+            os.system("pip install base64")
+            os.system("clear")
+            print("Lütfen tekrar deneyin")
+            self.usage()
+    
+    def exit(self):
+        print("çıkılıyor")
+        sys.exit()
+    
+    def usage(self):
+        print("""
+         _                     _    _  _
+        | |__   __ _ ___  ___ / /_ | || |
+        | '_ \ / _` / __|/ _ \ '_ \| || |_
+        | |_) | (_| \__ \  __/ (_) |__   _|
+        |_.__/ \__,_|___/\___|\___/   |_|
 
+                 """)
+    
+        print(f'{sys.argv[0]} -e or --encode <"encode"> >>>> şifreler')
+        print(f'{sys.argv[0]} -d or --decode <"decode"> >>>> şifreyi çözer')
+        print(f"{sys.argv[0]} -fe or --fileencode <encode.txt> >>>> dosyadaki karakteri şifreler")
+        print(f"{sys.argv[0]} -fd or --filedecode <decode.txt> >>>> dosyadaki şifreyi çözer")
+        sys.exit(1)
 
-	print(f'{sys.argv[0]} -e or --encode <"encode"> >>>> şifreler')
-	print(f'{sys.argv[0]} -d or --decode <"decode"> >>>> şifreyi çözer')
-	print(f"{sys.argv[0]} -fe or --fileencode <encode.txt> >>>> dosyadaki karakteri şifreler")
-	print(f"{sys.argv[0]} -fd or --filedecode <decode.txt> >>>> dosyadaki şifreyi çözer")
-	sys.exit(1)
+    def encode_string(self, input_string):
+        karakter = input_string.encode("utf-8")
+        göster = base64.b64encode(karakter)
+        return göster.decode("utf-8")
 
-if len(sys.argv) <3:
-	usage()
-	sys.argv(1)
+    def decode_string(self, encoded_string):
+        karakter1 = base64.b64decode(encoded_string)
+        return karakter1.decode("utf-8")
 
-elif sys.argv[1] in ["-e","--encode"]:
-	karakter = sys.argv[2]
-	çevir = karakter.encode("utf-8")
-	göster = base64.b64encode(çevir)
-	string = göster.decode("utf-8")	
-	print(string)
+    def encode_file(self, file_path):
+        try:
+            with open(file_path, 'r') as dosya:
+                for oku in dosya:
+                    string = self.encode_string(oku)
+                    print(string)
+        except FileNotFoundError:
+            print("böyle bir dosya yok")
 
-elif sys.argv[1] in ["-d","--decode"]:
-        karakter1 = base64.b64decode(sys.argv[2])
-        print(karakter1.decode("utf-8"))
+    def decode_file(self, file_path):
+        try:
+            with open(file_path, 'r') as dosya:
+                for oku in dosya:
+                    string = self.decode_string(oku)
+                    print(string)
+        except FileNotFoundError:
+            print("böyle bir dosya yok")
 
-elif sys.argv[1] in ["-fe","--fileencode"]:
-	try:
-		dosya = open(sys.argv[2])
-		aç = dosya.readlines()
-		for oku in aç:
-			karakter = oku.encode("utf-8")
-			göster = base64.b64encode(karakter)
-			string = göster.decode("utf-8")
-			print(string)
-	except FileNotFoundError:
-		print("böyle bir dosya yok")
-elif sys.argv[1] in ["-fd","--filedecode"]:
-	try:
-		dosya = open(sys.argv[2])
-		aç = dosya.readlines()
-		for oku in aç:
-			karakter = base64.b64decode(oku)
-			print(karakter.decode("utf-8"))
-	except FileNotFoundError:
-		print("böyle bir dosya yok")
+    def main(self):
+        parser = argparse.ArgumentParser(description="Base64 Encoder/Decoder")
+        parser.add_argument('-e', '--encode', type=str, help='Encode a string')
+        parser.add_argument('-d', '--decode', type=str, help='Decode a string')
+        parser.add_argument('-fe', '--fileencode', type=str, help='Encode a file')
+        parser.add_argument('-fd', '--filedecode', type=str, help='Decode a file')
 
-else:
-	print(f"Girdiğiniz panametre ({sys.argv[1]}) anlaşılmadı")
-	usage()
-	çık()
+        args = parser.parse_args()
 
+        if args.encode:
+            encoded_string = self.encode_string(args.encode)
+            print(encoded_string)
+        elif args.decode:
+            decoded_string = self.decode_string(args.decode)
+            print(decoded_string)
+        elif args.fileencode:
+            self.encode_file(args.fileencode)
+        elif args.filedecode:
+            self.decode_file(args.filedecode)
+        else:
+            print(f"Girdiğiniz parametre ({sys.argv[1]}) anlaşılmadı")
+            self.usage()
+            self.exit()
+
+if __name__ == "__main__":
+    base64_tool = Base64EncoderDecoder()
+    base64_tool.main()
